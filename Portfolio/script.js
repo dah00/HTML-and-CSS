3638123767ce639d13e714bed58750c495f9e1db
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2023-01-24 19:56:10 -0800

    Completed the Skills section using flexbox to make it responsive

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -23,3 +23,13 @@ const navSlide = () => {
 }
 
 navSlide();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
